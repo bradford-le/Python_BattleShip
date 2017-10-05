49a635cdf3f74fdf470762a3cdca6df40391dc9e
Brad Le <brad.le444@gmail.com>
2017-10-04 21:49:15 -0700

    Print board vertically in correct aspect

--- battleship.py
+++ battleship.py
@@ -1,8 +1,11 @@
 # Here is a python script for playing 1 player BattleShip
 
 board = []
-
 for i in range(5):
-  board.append(["O"]*5)
-
-print board
+  board.append(['O'] * 5)
+  
+def print_board(board_in):
+  for row in board_in:
+    print row
+    
+print_board(board)
